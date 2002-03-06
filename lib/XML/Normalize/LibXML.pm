33e453955df81e73c7a6e80262cdcf9eb039cea0
Petr Pajas <pajas@matfyz.cz>
2002-03-06 14:33:25 +0000

    Import of PAJAS/XML-XUpdate-LibXML-0.1 from CPAN.
    gitpan-cpan-distribution: XML-XUpdate-LibXML
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         PAJAS/XML-XUpdate-LibXML-0.1.tar.gz
    gitpan-cpan-author:       PAJAS
    gitpan-cpan-maturity:     released

---
+++ lib/XML/Normalize/LibXML.pm
@@ -0,0 +1,129 @@
+# $Id: LibXML.pm,v 1.1 2002/03/06 10:55:50 pajas Exp $
+
+package XML::Normalize::LibXML;
+
+use strict;
+use Exporter;
+use XML::LibXML;
+use vars qw(@ISA @EXPORT_OK $VERSION);
+
+BEGIN {
+  @ISA = qw(Exporter);
+  @EXPORT_OK = qw(trim xml_normalize xml_strip_whitespace);
+  $VERSION='0.1'
+}
+
+sub trim {
+  my ($text)=@_;
+  $text=~s/^\s*//;
+  $text=~s/\s*$//;
+  return $text;
+}
+
+sub xml_normalize {
+  my ($node) = @_;
+  my $prev = undef;	# previous Text node
+
+  foreach my $node ($node->childNodes()) {
+    my $type = $node->nodeType;
+    if ($type == XML_TEXT_NODE) {
+      if ($prev) {
+	$prev->setData($prev->getData().$node->getData());
+	$node->unbindNode();
+      } else {
+	$prev=$node;
+      }
+    } else {
+      $prev = undef;
+      if ($type == XML_ELEMENT_NODE) {
+	xml_normalize($node);
+      }
+    }
+  }
+}
+
+sub xml_strip_whitespace_text_node {
+  my ($node)=@_;
+  if ($node->nodeType() == XML_TEXT_NODE) {
+    $node->setData(trim($node->getData()));
+  }
+}
+
+sub xml_strip_whitespace_attributes {
+  my ($node)=@_;
+  if ($node->nodeType() == XML_ELEMENT_NODE) {
+    foreach my $attr ($node->attributes()) {
+      $node->setValue(trim($node->getValue()));
+    }
+  }
+}
+
+sub xml_strip_whitespace {
+  my ($dom, $strip_attributes)=@_;
+  xml_normalize($dom);
+  if ($strip_attributes) {
+    $dom->iterator(sub {
+		     xml_strip_whitespace_text_node($_[0]);
+		     xml_strip_whitespace_attributes($_[0]);
+		   });
+  } else {
+    $dom->iterator(\&xml_strip_whitespace_text_node);
+  }
+}
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+XML::LibXML::Normalize - simple whitespace striping functions
+
+=head1 SYNOPSIS
+
+use XML::LibXML::Normalize qw(trim xml_normalize xml_strip_whitespace);
+
+$greeting=trim("   hallo world    ");  # returns "hallo world"
+xml_normalize($dom->getDocumentElement());
+xml_strip_whitespace($dom->getDocumentElement());
+
+=head1 DESCRIPTION
+
+This module provides simple white-space striping and text-node
+normalizing functions.
+
+=head2 C<trim($string)>
+
+Returns the string with any whitespace occuring at its beginning or
+end removed.
+
+=head2 C<xml_normalize($dom)>
+
+Puts all Text nodes in the full depth of the sub-tree underneath this
+Element into a normal form where only markup (e.g., tags,
+comments, processing instructions, CDATA sections, and entity
+references) separates Text nodes, i.e., there are no adjacent Text
+nodes. This can be used to ensure that the DOM view of a document is
+the same as if it were saved and re-loaded, and is useful when
+operations (such as XPointer lookups) that depend on a particular
+document tree structure are to be used.
+
+=head2 C<xml_strip_whitespace($dom [,$include_attributes])>
+
+Normalizes the subtree and trims whitespace from all Text nodes within
+the subtree. If the optional argument $include_attributes is defined
+and non-zero, this function trims whitespace also from all Attribute
+nodes.
+
+=head1 AUTHOR
+
+Petr Pajas, pajas@matfyz.cz
+
+=head1 SEE ALSO
+
+L<XML::LibXML>
+
+=cut
---
+++ lib/XML/XUpdate/LibXML.pm
@@ -0,0 +1,311 @@
+# $Id: LibXML.pm,v 1.1 2002/03/06 10:55:50 pajas Exp $
+
+package XML::XUpdate::LibXML;
+
+use XML::LibXML;
+use strict;
+use vars qw(@ISA $debug $VERSION);
+
+BEGIN {
+  $debug=0;
+  $VERSION = '0.1';
+}
+
+sub strip_space {
+  my ($text)=@_;
+  $text=~s/^\s*//;
+  $text=~s/\s*$//;
+  return $text;
+}
+
+sub new {
+  my $class=(ref($_[0]) || $_[0]);
+  return bless [{},"http://www.xmldb.org/xupdate"], $class;
+}
+
+sub _set_var {
+  my ($self,$name,$value)=@_;
+  $self->[0]->{$name}=$value;
+}
+
+sub _get_var {
+  my ($self,$name)=@_;
+  return $self->[0]->{$name};
+}
+
+sub set_namespace {
+  my ($self,$URI)=@_;
+  $self->[1]=$URI;
+}
+
+sub namespace {
+  my ($self)=@_;
+  return $self->[1];
+}
+
+sub process {
+  my ($self,$dom,$updoc)=@_;
+  return unless ref($self);
+
+  print STDERR "Updating $dom\n" if $debug;
+  foreach my $command ($updoc->getDocumentElement()->childNodes()) {
+    if ($command->nodeType == XML_ELEMENT_NODE) {
+      if ($command->getNamespaceURI() eq $self->namespace()) {
+	print STDERR "applying ",$command->toString(),"\n" if $debug;
+	$self->xupdate_command($dom,$command);
+      } else {
+	print STDERR "Ignorint element ",$command->toString(),"\n" if $debug;
+      }
+    }
+  }
+}
+
+sub get_text {
+  my ($self,$node)=@_;
+  my $text="";
+  foreach ($node->childNodes()) {
+    if ($_->nodeType() == XML_TEXT_NODE ||
+	$_->nodeType() == XML_CDATA_SECTION_NODE) {
+      $text.=$_->getData();
+    }
+  }
+  return strip_space($text);
+}
+
+sub append {
+  my ($self,$node,$results)=@_;
+  foreach (@$results) {
+    if ($_->nodeType == XML_ATTRIBUTE_NODE) {
+      $node->setAttributeNS($_->getNamespaceURI,$_->getName(),$_->getValue);
+    } else {
+      $node->appendChild($_);
+    }
+  }
+}
+
+sub insert_after {
+  my ($self,$node,$results)=@_;
+  foreach (reverse @$results) {
+    $node->parentNode()->insertAfter($_,$node);
+  }
+}
+
+sub insert_before {
+  my ($self,$node,$results)=@_;
+  foreach (@$results) {
+    $node->parentNode()->insertBefore($_,$node);
+  }
+}
+
+sub append_child {
+  my ($self,$node,$results,$child)=@_;
+
+  if ($child ne "") {
+    $child=$node->findvalue($child) unless $child =~/^\s*\D+\s*$/;
+    my @children=$node->childNodes();
+    my $after=$children[$child-1];
+    if ($child>1 and $after) {
+      $self->insert_after($after,$results);
+    } elsif (@children and $after) {
+      $self->insert_before($children[0],$results);
+    } else {
+      $self->append($node,$results);
+    }
+  } else {
+    $self->append($node,$results);
+  }
+}
+
+sub update {
+  my ($self,$node,$results)=@_;
+
+  if ($node->nodeType == XML_TEXT_NODE ||
+      $node->nodeType == XML_CDATA_SECTION_NODE) {
+    $self->insert_after($node,$results);
+    $node->unbindNode();
+  } elsif ($node->nodeType == XML_ATTRIBUTE_NODE ||
+	   $node->nodeType == XML_PI_NODE) {
+    $node->setValue(strip_space(join "", map { $_->toString() } @$results));
+  } elsif ($node->nodeType == XML_ELEMENT_NODE) {
+    foreach ($node->childNodes()){
+      $_->unbindNode();
+    }
+    $node->append($node,$results);
+  }
+}
+
+sub remove {
+  my ($self, $node)=@_;
+  $node->unbindNode();
+}
+
+sub rename {
+  my ($self,$node,$name)=@_;
+  $node->setName($name);
+}
+
+sub process_instructions {
+  my ($self,$doc, $command)=@_;
+  my @result=();
+  foreach my $inst ($command->childNodes()) {
+    print STDERR "instruction ",$command->toString(),"\n" if $debug;
+    if ( $inst->nodeType == XML_ELEMENT_NODE ) {
+      if ( $inst->getLocalName() eq 'element' ) {
+	my $new=$doc->createElementNS(
+				      $inst->getAttribute('namespace'),
+				      $inst->getAttribute('name')
+				     );
+	$self->append($new,$self->process_instructions($doc,$inst));
+	push @result,$new;
+      } elsif ( $inst->getLocalName() eq 'attribute' ) {
+	push @result,$doc->createAttributeNS(
+					     $inst->getAttribute('namespace'),
+					     $inst->getAttribute('name'),
+					     $self->get_text($inst)
+					    );
+      } elsif (  $inst->getLocalName() eq 'text' ) {
+	push @result,$doc->createTextNode($self->get_text($inst));
+      } elsif ( $inst->getLocalName() eq 'processing-instruction' ) {
+	push @result,$doc->createProcessingInstruction(
+						       $inst->getAttribute('name'),
+						       $self->get_text($inst)
+						      );
+      } elsif ( $inst->getLocalName() eq 'value-of' ) {
+	my $value=$doc->findvalue($self->get_select($inst))->to_literal();
+	push @result,$doc->createTextNode($value);
+      } else {
+	# not in XUpdate DTD but in examples of XUpdate WD
+	push @result,$doc->importNode($inst) 
+	  unless ($inst->getNamespaceURI eq $self->namespace());
+      }
+    } elsif ( $inst->nodeType == XML_CDATA_SECTION_NODE ||
+	      $inst->nodeType == XML_TEXT_NODE) {
+      push @result,$doc->importNode($inst);
+    }
+  }
+  return \@result;
+}
+
+sub get_select {
+  my ($self,$node)=@_;
+  my $select=$node->getAttribute('select');
+  $select=~s/\$([A-Za-z0-9._\-:]+)/_get_var($1)/e; # TODO: complete NMTOKEN match
+  return $select;
+}
+
+sub xupdate_command {
+  my ($self,$dom,$command)=@_;
+  my $doc=$dom->getOwnerDocument();
+
+  return unless ($command->getType == XML_ELEMENT_NODE);
+  my $select=$self->get_select($command);
+  if ($command->getLocalName() eq 'variable') {
+    if ($select ne "") {
+      $self->_set_var($command->getAttribute('name'),$select);
+    } else {
+      $self->_set_var($self->get_text($command));
+    }
+  } else {
+    if ($select ne "") {
+      my @refnodes=$dom->findnodes($select);
+      if (@refnodes) {
+	if ($command->getLocalName eq 'insert-after') {
+	  my $results=$self->process_instructions($doc,$command);
+	  $self->insert_after($refnodes[0],$results);
+	} elsif ($command->getLocalName eq 'insert-before') {
+	  my $results=$self->process_instructions($doc,$command);
+	  $self->insert_before($refnodes[0],$results);
+	  my $results=$self->process_instructions($doc,$command);
+	} elsif ($command->getLocalName eq 'append') {
+	  my $results=$self->process_instructions($doc,$command);
+	  my $child=$command->getAttribute('child');
+	  $self->append_child($refnodes[0],$results,$child);
+	} elsif ($command->getLocalName eq 'update') {
+	  my $results=$self->process_instructions($doc,$command);
+	  # Well, XUpdate WD is not very specific about this.
+          # The content of this element should be PCDATA only.
+          # I'm extending WD by allowing instruction list.
+	  $self->update($refnodes[0],$results);
+	} elsif ($command->getLocalName eq 'remove') {
+	  $self->remove($refnodes[0]);
+	} elsif ($command->getLocalName eq 'rename') {
+	  $self->rename($refnodes[0],$self->get_text($command));
+	}
+      }
+    }
+  }
+}
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+XML::XUpdate::LibXML - Simple implementation of XUpdate format
+
+=head1 SYNOPSIS
+
+use XML::LibXML;
+use XML::XUpdate::LibXML;
+
+$parser  = XML::LibXML->new();
+$dom     = $parser->parse_file("mydoc.xml");
+$actions = $parser->parse_file("update.xml");
+
+$xupdate = XML::LibXML::XUpdate->new();
+$xupdate->process($dom->getDocumentElement(), $actions);
+print $dom->toString(),"\n";
+
+=head1 DESCRIPTION
+
+This module implements the XUpdate format described in XUpdate Working
+Draft from 2000-09-14 (http://www.xmldb.org/xupdate/xupdate-wd.html).
+The implementation is based on XML::LibXML DOM API.
+
+
+=head2 C<new>
+
+    my $xupdate = XML::LibXML::XUpdate->new();
+
+Creates a new XUpdate object. You may use this object to update
+several different DOM trees using several different XUpdate
+descriptions. The advantage of it is that an xupdate object remembers
+values all variables declared in XUpdate documents.
+
+=head2 C<$xupdate->process($document_dom,$xupdate_dom)>
+
+This function takes two DOM trees as its arguments. It works by
+updating the first tree according to all XUpdate commands included in
+the second one. All XUpdate commands must be children of the root
+element of the second tree and must all belong to XUpdate namespace
+"http://www.xmldb.org/xupdate". The namespace URI may be changed
+with set_namespace method.
+
+=head2 C<$xupdate->set_namespace($URI)>
+
+You may use this method to change the namespace of XUpdate elements.
+The default namespace is "http://www.xmldb.org/xupdate".
+
+=head2 C<$xupdate->namespace()>
+
+Returns XUpdate namespace URI used by XUpdate processor to identify
+XUpdate commands.
+
+=head2 EXPORT
+
+None.
+
+=head1 AUTHOR
+
+
+Petr Pajas, pajas@matfyz.cz
+
+=head1 SEE ALSO
+
+L<XML::LibXML>
+
+=cut
